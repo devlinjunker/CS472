b65ef3a1875988b9adb1328da13eca826486a59f
Philenox <philenox@gmail.com>
2013-10-17 16:43:04 -0700

    added feature file

---
+++ lab1/feature.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <stdlib.h>
+
+union Data
+{
+	char str[8];
+	double d;
+	long l;
+};
+
+
+void feature(union Data newData)
+{
+	int dummy;
+	printf("double mantissa: %f\n", frexp(newData.d, &dummy));
+	printf("double exponent: %d\n", dummy);
+	dummy = newData.d < 0 ? -1 : 1; 
+	printf("double sign: %d\n", dummy);
+	printf("long value: %ld\n", newData.l);
+	dummy = newData.l < 0 ? -1 : 1;
+	printf("long sign: %d\n", dummy);
+	printf("char array: \n");
+
+	for(int i = 0; i < 8; i++)
+	{
+		printf("%c\n", newData.str[i]);
+	}
+}
+
+int main()
+{
+
+	union Data user_data;
+	user_data.l = 74659376584;
+	feature(user_data);
+}
