ba98f3293d930ade24f2bb532f4d4094fd47beb6
devlinjunker <devlin.junker@gmail.com>
2013-10-17 19:49:41 -0700

    finished frexp, denormalized numbers are done, added some comments

--- lab1/frexp.c
+++ lab1/frexp.c
@@ -12,7 +12,6 @@
 
 #define F64_MANT_MASK 0x000FFFFFFFFFFFFF
 #define F64_MANT_SHIFT 12
-//#define F64_GET_MANT(fp) (long) ( F64_GET_MANT_HI(fp) | F64_GET_MANT_MED(fp) | F64_GET_MANT_LOW(fp) )
 #define F64_GET_MANT(fp) *(long*)&fp & F64_MANT_MASK
 
 #define F64_GET_MANT_HI(fp) ( (long)(((int)BYTE_ARR(x)[6] & 0x0F) << 8 | ((int)BYTE_ARR(x)[5] & 0xFF)) << 40 )
@@ -26,47 +25,57 @@ double frexp(double x, int *exp)
 
     double ret = 1;
 
-    // Check if x is equal to zero
-    if(x - 0 < 0.0000001 & x - 0 > -0.0000001){
-        exp = 0;
-        ret = 0;
-    }else{
-        *exp = F64_GET_EXP(x) ;
-        stored_mant = F64_GET_MANT(x);
-        
-        // TODO: Check for denormalized numbers;
-
-        temp = stored_mant << 12;
-
-        int new_val = 0;
-        double pow2 = 0.5;
-
-        while(temp != 0){
-
-            new_val = ((temp & (((unsigned long)1)<<63)) >> 63); 
-
-            ret += new_val * pow2;
-
-            temp <<= 1;
-            pow2 = pow2/2;;
-        }
-
-        printf("%f \n", ret);
-        printf("%d \n", *exp);
-
-        while(ret > 1 || ret < 0.5){
-            if(ret > 1){
-                ret = ret / 2; 
-                *exp = *exp + 1;
-            }else if(ret < 0.5){
-                ret = ret * 2;
-                *exp = *exp - 1;
-            }
-
-        }
+    *exp = F64_GET_EXP(x) ;
+    stored_mant = F64_GET_MANT(x);
+    
+    // Check if exponent is all 1s
+    if( (*exp+F64_EXP_BIAS) & (((unsigned long)0)-1) == (((unsigned long)0)-1)){
+        ret = x;
+        goto end;
     }
 
-    return ret;
+    // Move bits all the way to the right to cover hole left by sign and exponent
+    temp = stored_mant << 12;
+
+    // Check if mantissa is 0
+    if(temp | (unsigned long)0 == 0){
+        ret = 0;
+        *exp = 0;
+        goto end;
+    }
+
+    int new_val = 0; // holds the most significant bit
+    double pow2 = 0.5; // starting power of 2
+
+    // while there are still 1s in the mantissa
+    while(temp != 0){
+
+        // grab most sig bit
+        new_val = ((temp & (((unsigned long)1)<<63)) >> 63); 
+
+        // multiply by power of 2 for that bit in the function
+        ret += new_val * pow2;
+
+        // shift out most significant bit
+        temp <<= 1;
+        // next power of 2 lower
+        pow2 = pow2/2;
+    }
+
+    // normalized fractions are between 0.5 and 1
+    while(ret > 1 || ret < 0.5){
+        if(ret > 1){
+            ret = ret / 2; 
+            *exp = *exp + 1;
+        }else if(ret < 0.5){
+            ret = ret * 2;
+            *exp = *exp - 1;
+        }
+
+    }
+
+    end:
+        return ret;
 }
 
 int main(int argc, char *argv[])
@@ -84,38 +93,16 @@ int main(int argc, char *argv[])
         x = strtod(argv[1], NULL);
         r = frexp(x, &exp);
         printf("value of %g, returns normalized fraction = %g and exponent = %d \n", x, r, exp);
-
-        //r = frexp(0.0/0.0, &exp);
-        //printf("value of %g, returns normalized fraction = %g and exponent = %d \n", 0.0/0.0, r, exp);
-
-        //r = frexp(8.9/0.0, &exp);
-        //printf("value of %g, returns normalized fraction = %g and exponent = %d \n", 8.9/0.0, r, exp);
-
-
-        printf("exponent: %d \n", F64_GET_EXP(x));
-        printf("mantissa: %lx \n", F64_GET_MANT(x));
-
-        long a = F64_GET_MANT(x) << 12;
-
-        long val = a << 1;
-
-        printf("mantissa shifted: %lx \n", a);
-        printf("shifted result: %lx \n", val);
-
-        val = a >> 2;
-
-        printf("mantissa shifted: %lx \n", a);
-        printf("shifted result: %lx \n", val);
-//        test = (unsigned char*)(((long)x) << 52);
-
-        printf("test: %hu \n", ( BYTE_ARR(x)[6] & 0x0F));
-        printf("test: %hu \n", ( BYTE_ARR(x)[5] & 0xFF));
-        printf("test: %hu \n", ( BYTE_ARR(x)[4] & 0xFF));
-        printf("test: %hu \n", ( BYTE_ARR(x)[3] & 0xFF));
-        printf("test: %hu \n", ( BYTE_ARR(x)[2] & 0xFF));
-        printf("test: %hu \n", ( BYTE_ARR(x)[1] & 0xFF));
-
-        printf("test (1 << 63) %lx", ((long)1)<<63);
+        
+        // denormalized values
+        r = frexp(4.0/0.0, &exp);
+        printf("value of %g, returns normalized fraction = %g and exponent = %d \n", 4.0/0.0, r, exp);
+        
+        r = frexp(-4.0/0.0, &exp);
+        printf("value of %g, returns normalized fraction = %g and exponent = %d \n", -4.0/0.0, r, exp);
+        
+        r = frexp(0.0/0.0, &exp);
+        printf("value of %g, returns normalized fraction = %g and exponent = %d \n", 0.0/0.0, r, exp);
     }   
 
     return 0;
